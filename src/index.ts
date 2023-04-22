9e284c25379e93f3f2d3e185003da8e2a0ec5e47
Evan Liu <evancoding@gmail.com>
2023-04-23 09:09:59 +1200

    🔨 Add the build npm script

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { writeToProfile } from 'karabiner-config'
+
+// Change 'Examples' to your Karabiner-Elements Profile name. Create a new profile if needed.
+writeToProfile('Examples', [
+  //
+])
