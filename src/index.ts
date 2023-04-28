070fc687444078ca378c659214e5ce3e1d1b8430
Evan Liu <evancoding@gmail.com>
2023-04-29 08:54:26 +1200

    📝 profile and mapDoubleTap() parameters

--- src/index.ts
+++ src/index.ts
@@ -110,3 +110,20 @@ writeToProfile('--dry-run', [
     mapDoubleTap(1).to('w', '⌘'),
   ]),
 ])
+
+/*
+Karabiner-Elements profile parameters can also be set by the 3rd parameter
+of writeToProfile('profileName', [ rules ], { params }). The default values are:
+
+// Karabiner-Elements parameters
+'basic.to_if_alone_timeout_milliseconds': 1000,
+'basic.to_if_held_down_threshold_milliseconds': 500,
+'basic.to_delayed_action_delay_milliseconds': 500,
+'basic.simultaneous_threshold_milliseconds': 50,
+'mouse_motion_to_scroll.speed': 100,
+
+// karabiner.ts only parameters
+// for mapDoubleTap()
+'double_tap.delay_milliseconds': 200,
+
+ */
